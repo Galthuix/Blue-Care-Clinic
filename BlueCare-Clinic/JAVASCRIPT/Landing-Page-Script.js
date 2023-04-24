e77b13a26f5f8f8fe882cf8f16eeabbf906f2519
Xyvn <jobertryancapillan@gmail.com>
2023-04-24 17:49:19 +0800

    Just wanna Update

---
+++ BlueCare-Clinic/JAVASCRIPT/Landing-Page-Script.js
@@ -0,0 +1,14 @@
+let mybutton = document.getElementById("Back-To-Top");
+window.onscroll = function() {topFunction};
+
+function topFunction() {
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
